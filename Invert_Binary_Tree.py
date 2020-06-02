0777a21e86d06991af3d0304bae4e85a2eaa9f74
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-06-02 19:27:39 +0530

    Create Invert_Binary_Tree.py

---
+++ Invert_Binary_Tree.py
@@ -0,0 +1,43 @@
+'''
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            queue = [root]
+            while queue:
+                temp = queue.pop(0)
+                temp.right, temp.left = temp.left, temp.right
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+        return root
