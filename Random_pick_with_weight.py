e7346777bd8b50c3e7d609ddff02a062d03fd18f
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-06-09 14:25:42 +0530

    Update Random_pick_with_weight.py

--- Random_pick_with_weight.py
+++ Random_pick_with_weight.py
@@ -39,23 +39,19 @@ class Solution:
             self.w[i] += self.w[i-1]
         
     def pickIndex(self) -> int:
-		
-		# this is where we pick the index
+
+	# this is where we pick the index
         N = random.uniform(0,1)
       
         flag = 1
         index = -1
         
-		# test each region of the numberline to see if N falls in it, if it 
-		# does not then go to the next index and check if N falls in it
-		# this is gaurenteed to break because of previous normalization
+	# test each region of the numberline to see if N falls in it, if it 
+	# does not then go to the next index and check if N falls in it
+	# this is gaurenteed to break because of previous normalization
         while flag:
             index +=1 
-           
-           
-     
             if N <= self.w[index]:
                 flag = 0
-        
-    
+
         return index
