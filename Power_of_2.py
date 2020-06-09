c2ed7ea144d5452f1f8cbf209005bdcbdd3ce1df
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-06-09 14:27:59 +0530

    Create Power_of_2.py

---
+++ Power_of_2.py
@@ -0,0 +1,29 @@
+'''
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true 
+Explanation: 20 = 1
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: 218
+Output: false
+'''
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==1:
+            return True
+        num=1
+        while(num<n):
+            num=num*2
+            if num==n:
+                return True
+        return False
