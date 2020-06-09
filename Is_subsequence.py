47be32aa9f96f356d79351c38ec5cd6e6526a52a
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-06-09 14:29:12 +0530

    Create Is_subsequence.py

---
+++ Is_subsequence.py
@@ -0,0 +1,41 @@
+'''
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+'''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        m=len(s)
+        n=len(t)
+        dp=[[0]*(m+1)]*(n+1)
+
+        for i in range(n):
+            for j in range(m):
+                if s[j]==t[i]:
+                    dp[i+1][j+1]=min(dp[i][j+1], dp[i+1][j])+1
+                else:
+                    dp[i+1][j+1]=max(dp[i][j+1], dp[i+1][j])
+                    
+        if dp[-1][-1]==len(s):
+            return True
+        else:
+            return False
+                    
